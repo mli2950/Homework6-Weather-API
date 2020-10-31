5d4bb4fdbf2a3a099d3be58e373be7369866ceaf
Michael Li <Mli2950@gmail.com>
2020-10-30 19:21:49 -0600

    Started history button functionality

--- Assets/script.js
+++ Assets/script.js
@@ -6,41 +6,44 @@ $("button").click(function () {
     $("p").empty();
     $("h3").empty();
 
-  var userInput = document.getElementById("cityInput").value;
-  var requestURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    userInput + "&units=imperial&APPID=" + APIKey;
+    var userInput = document.getElementById("cityInput").value;
+    var requestURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        userInput + "&units=imperial&APPID=" + APIKey;
 
-  $.ajax({
-    url: requestURL,
-      method: "GET",
-      success: function (cityJSON) {
-          console.log(cityJSON);
-          var historyItem = JSON.parse(localStorage.getItem(localStorage.key("userInput")))
-          $("#cityNameDisplay").append(cityJSON.name + " - " +  m)
-          $("#temperatureDisplay").append("Temperature: " + cityJSON.main.temp + " Degrees")
-          $("#humidityDisplay").append("Humidity: " + cityJSON.main.humidity + "%")
-          $("#windSpeedDisplay").append("Wind Speed: " + cityJSON.wind.speed + " Miles Per Hour")
-          $("#skiesDisplay").append("Skies are: " + cityJSON.weather[0].main)
-          var userInput = $("#cityInput").val()
-          storedCities.push(userInput)
-          localStorage.setItem("userInput", JSON.stringify(storedCities))
+    $.ajax({
+        url: requestURL,
+        method: "GET",
+        success: function (cityJSON) {
+            console.log(cityJSON);
+            var historyItem = JSON.parse(localStorage.getItem(localStorage.key("userInput")))
+            $("#cityNameDisplay").append(cityJSON.name + " - " + m)
+            $("#temperatureDisplay").append("Temperature: " + cityJSON.main.temp + " Degrees")
+            $("#humidityDisplay").append("Humidity: " + cityJSON.main.humidity + "%")
+            $("#windSpeedDisplay").append("Wind Speed: " + cityJSON.wind.speed + " Miles Per Hour")
+            $("#skiesDisplay").append("Skies are: " + cityJSON.weather[0].main)
+            var userInput = $("#cityInput").val()
+            storedCities.push(userInput)
+            localStorage.setItem("userInput", JSON.stringify(storedCities))
           
-          
-              var searchItem = document.createElement("input");
-              searchItem.setAttribute("type", "text");
-              searchItem.setAttribute("readonly", "true");
-              searchItem.setAttribute("class", "form-contronl d-block");
-              searchItem.setAttribute("value", userInput)
-              searchItem.setAttribute("click", function () {
-                //   TODO: Implement returning weather when clicking history
-              })
-              $("#history").append(searchItem);
-          
+            if (historyItem !== null) {
+                var searchItem = document.createElement("input");
+                searchItem.setAttribute("type", "text");
+                searchItem.setAttribute("readonly", "true");
+                searchItem.setAttribute("class", "form-control d-block");
+                searchItem.setAttribute("id", "searchItem")
+                searchItem.setAttribute("value", storedCities[storedCities.length-1])
+                
+            
+                $("#history").append(searchItem);
+            }
         
-      }
-  });
-});
+        }
+    }
+    )
+})
+
+
 
 function showStorage() {
     var historyItem = JSON.parse(localStorage.getItem(localStorage.key("userInput")));
@@ -49,11 +52,9 @@ function showStorage() {
             var searchItem = document.createElement("input");
               searchItem.setAttribute("type", "text");
               searchItem.setAttribute("readonly", "true");
-              searchItem.setAttribute("class", "form-contronl d-block");
+              searchItem.setAttribute("class", "form-control d-block");
+              searchItem.setAttribute("id", "searchItem");
               searchItem.setAttribute("value", historyItem[i])
-              searchItem.setAttribute("click", function () {
-                //   TODO: Implement returning weather when clicking history
-              })
               $("#history").append(searchItem);
         }
     }
@@ -61,4 +62,25 @@ function showStorage() {
 
 showStorage();
 
-
+$(".form-control.d-block").click(function () {
+    $("p").empty();
+    $("h3").empty();
+    var historyInput = this.value
+    
+    console.log(historyInput)
+        $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" +
+        historyInput + "&units=imperial&APPID=" + APIKey,
+        method: "GET",
+        success: function (cityJSON) {
+            console.log(cityJSON);
+            
+            $("#cityNameDisplay").append(cityJSON.name + " - " + m)
+            $("#temperatureDisplay").append("Temperature: " + cityJSON.main.temp + " Degrees")
+            $("#humidityDisplay").append("Humidity: " + cityJSON.main.humidity + "%")
+            $("#windSpeedDisplay").append("Wind Speed: " + cityJSON.wind.speed + " Miles Per Hour")
+            $("#skiesDisplay").append("Skies are: " + cityJSON.weather[0].main)
+            
+        }
+    })
+})
