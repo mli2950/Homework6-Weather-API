98ddbf70a683a3e4f9a614e0a5463c58f846a44b
Michael Li <Mli2950@gmail.com>
2020-11-04 19:29:25 -0700

    Commenting JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,49 @@
+// Hide the 5 Day forecast cards until called
 $(".forecast").hide();
+// Moment.JS call and formatting
 var m = moment().format("LLL");
+// API key to be concatenated into API call URL's
 const APIKey = "1a782e7aafcfa4684a2e6ac3c8810cba";
+// Empty array to store values from local storage
 var storedCities = [];
 
 // Event listener for pressing enter button while typing in input field
 var enter = document.getElementById("cityInput");
 enter.addEventListener("keyup", function () {
   if (event.keyCode == 13) {
+    // Empty all weather information elements
     $("p").empty();
       $("h3").empty();
-      $("span").empty();
+    $("span").empty();
+    // If there is anything in local Storage, place those values inside the empty array
     if (localStorage.getItem("userInput") !== null) {
       storedCities = JSON.parse(localStorage.getItem("userInput"));
     }
-
+    // Assign the users input from the input element to the userInput variable
     var userInput = document.getElementById("cityInput").value;
+    // store the API call url in a variable, concatenated with variables to complete the link
     var requestURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       userInput +
       "&units=imperial&APPID=" +
       APIKey;
-
+    // Ajax call to get data from the API
     $.ajax({
       url: requestURL,
       method: "GET",
+      // If the call is successful, run this function
       success: function (cityJSON) {
         console.log(cityJSON);
-
+        // Grab the icon code from the data return, and assign it to a variable
         var getfivedayIcon = cityJSON.weather[0].icon;
         fivedayIcon =
           "http://openweathermap.org/img/wn/" + getfivedayIcon + "@2x.png";
 
+        // Grab the array from local Storage, and assign it to the historyItem variable
         var historyItem = JSON.parse(
           localStorage.getItem(localStorage.key("userInput"))
         );
+        // Appending current weather data to the page by grabbing it from the data return
         $("#cityNameDisplay").append(
           cityJSON.name + " - " + m + "<img src=" + fivedayIcon + ">"
         );
@@ -46,10 +56,13 @@ enter.addEventListener("keyup", function () {
         $("#windSpeedDisplay").append(
           "Wind Speed: " + cityJSON.wind.speed + " Miles Per Hour"
         );
+
+        // Grab the value from the input Element, and assign it a variable. Push that value into the array, and store that array in local storage.
         var userInput = $("#cityInput").val();
         storedCities.push(userInput);
         localStorage.setItem("userInput", JSON.stringify(storedCities));
 
+        // if there is any values in historyItem, append the weather information for that value.
         if (historyItem !== null) {
           var searchItem = document.createElement("input");
           searchItem.setAttribute("type", "text");
@@ -76,18 +89,19 @@ enter.addEventListener("keyup", function () {
   }
 });
 
-// Click event listener for search button
+// When the search button is clicked, run the getweather function
 $("button").click(getWeather())
 
 // showHistory function
 function showHistory(x) {
+  // Empty appended weather data
     $("p").empty();
     $("h3").empty();
     $("span").empty();
   if (localStorage.getItem("userInput") !== null) {
     storedCities = JSON.parse(localStorage.getItem("userInput"));
   }
-
+  // Userinput is set to the lat and lon values that are passed to the function
   var userInput = x;
   var requestURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -196,7 +210,7 @@ function getWeather(x) {
   });
 };
 
-// showStorage function
+// showStorage function that is called when page is loaded
 function showStorage() {
   var historyItem = JSON.parse(
     localStorage.getItem(localStorage.key("userInput"))
@@ -224,9 +238,10 @@ function showStorage() {
     }
   }
 }
-
+// showStorage function call
 showStorage();
 
+// Function to get the UV index when called
 function getUV(lat, lon) {
   $.ajax({
     url:
@@ -252,13 +267,17 @@ function getUV(lat, lon) {
   });
 }
 
+// Function to display 5 day forecast when called
 function fiveDay(lat, lon) {
-    $(".forecast").show();
+  // Show the forecast cards
+  $(".forecast").show();
+  // Empty the forecast cards so info is not appended over other info
   $("#forecast1").empty();
   $("#forecast2").empty();
   $("#forecast3").empty();
   $("#forecast4").empty();
   $("#forecast5").empty();
+  // AJAX call
   $.ajax({
     url:
       "https://api.openweathermap.org/data/2.5/onecall?lat=" +
